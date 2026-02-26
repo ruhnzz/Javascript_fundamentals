10994e28f483f4417161e34be358eac03c8e1554
ruhnzz <imruheena18@gmail.com>
2026-02-26 13:26:11 +0530

    updated

---
+++ Operators.js
@@ -0,0 +1,36 @@
+// 3. OPERATORS
+
+//Arthematic
+// + - * / %  **
+let a = 1
+let b = 2
+
+
+//Comparision
+// > < >= <= != == ===
+
+
+
+//Logical
+// && || !
+
+
+//Assigment
+ // = += -= *= /= %=
+
+
+//Unary Operator
+// typeof ++ --
+// ++a => preincrement
+// a++ => postincrement
+// --a => predecrement
+// a-- => postdecrement 
+
+
+// Teranary Operator
+// condition?true:false
+
+
+// typeof vs instanceof 
+
+function add(){}
