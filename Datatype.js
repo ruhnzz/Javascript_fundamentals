0f7e6c2a1e35019049372b548badc13fbb18aee4
ruhnzz <imruheena18@gmail.com>
2026-02-26 13:02:47 +0530

    first commit

---
+++ Datatype.js
@@ -0,0 +1,53 @@
+// 2. DATA TYPE AND TYPE SYSTEMS
+
+// Primitive
+// -> real values that can be copied 
+
+// String => '' "" ``
+// Number => 1 1.2
+// Boolean => true and false
+// null => nothing 
+// undefine
+// Symbol 
+// let obj = {id:1 , name:"abc"} 
+// let myId = Symbol("id")
+// obj[myId]=100
+// bigInt
+// let a = 9007199254740991n
+
+// Reference
+// we can copy the reference of value not the real value 
+// Arrays
+// let a = []
+// //object
+// let = {}
+// // function
+// function add(){
+//     //code
+// }
+
+
+
+
+//Dynamic Typing
+let a = 2
+a = 3
+a = "hello"
+
+
+
+// type of quirks
+// console.log(typeof(NaN))
+
+
+// type corecion
+// console.log("5"+1)
+// console.log("5"-1)
+
+
+
+
+// truthy and falsy 
+// false = 0 , null, undefine, false ,NaN,""
+
+
