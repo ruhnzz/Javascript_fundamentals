e14d15d4e55e051a566e7592217c9fd367d79e47
ruhnzz <imruheena18@gmail.com>
2026-02-27 17:39:36 +0530

    first commit

---
+++ Loops.js
@@ -0,0 +1,54 @@
+// 5.LOOPS
+
+// print num 1 - 5
+
+// for
+// for(start;condition;change){}
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// while
+// start while(cond) {change}
+// let i = 0
+// while(i<5){
+//     i++;
+//     console.log(i);
+// }
+
+
+
+// do While
+// start do{change}while(cond);
+// let i = 0
+// do{
+//     i++;
+//     console.log(i);
+// }while(i<5);
+
+
+// break and continue
+// for(let i=1;i<=5;i++){
+//     if(i==3){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+let a = ['a','b','c'];
+// for in 
+for (let i in a){
+    console.log(i)
+}
+
+// for of 
+for (let i of a){
+    console.log(i)
+}
+for(let i =0;i<a.length;i++){
+    console.log(i,a[i]);
+}
+
+
---
+++ functions.js
@@ -0,0 +1,31 @@
+// functions
+
+// function declaration
+
+//function expression
+
+// arrow function
+
+//printing dynamic value 
+
+// parameters vs arguments
+
+//passing default values
+
+//rest and spread parameters
+
+//first class fucntion
+
+// Higher Order Function HOF
+
+// pure vs impure function
+
+//closures
+
+// lexical scoping
+
+//Immediately Invoked Function Expression IIFE
+
+//hoisting
+
+
